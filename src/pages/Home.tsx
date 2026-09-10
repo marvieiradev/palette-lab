fefdb5dd44a73fba8cfcfb32edc2c3fdc4e0e6cc
Marcos Vieira <marcosvieira482@gmail.com>
2026-09-10 20:12:10 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+            <p className="mt-4 text-lg text-gray-600">This is the main landing page of the application.</p>
+        </div>
+    );
+}
