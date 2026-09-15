677c090c3efe0546b6801f196fc630098492af43
Marcos Vieira <marcosvieira482@gmail.com>
2026-09-14 22:44:32 -0300

    feat: adds tooltip support using Radix UI and refactors the App component to include Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import { Toaster } from "sonner";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 
 function App() {
   return (
-    <Home />
-  )
+    <TooltipProvider>
+      <Toaster theme="dark" position="bottom-right" />
+      <Home />
+   </TooltipProvider>
+  );
 }
 
-export default App
+export default App;
